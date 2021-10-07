e723c9f98a3289ec7beccb4a51ba08885cdbde67
Antonio Sucre <antonio.sucre@indikatore.com>
2021-10-07 05:03:33 -0300

    add-feature-ordenar

--- src/listar/listar-tarefas.js
+++ src/listar/listar-tarefas.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { A } from 'hookrouter';
-import { Table, Form } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import ItensListaTarefas from './itens-lista-tarefas';
 import Paginacao from './paginacao';
+import Ordenacao from './ordenacao';
 
 function ListarTarefas() {
     const ITENS_POR_PAGINA = 3;
@@ -13,11 +14,20 @@ function ListarTarefas() {
     const [carregarTarefas, setCarregarTarefas] = useState(true);
     const [totalItems, setTotalItems] = useState(0);
     const [paginaAtual, setPaginaAtual] = useState(1);
+    const [ordenarAsc, setOrdenarAsc] = useState(false);
+    const [ordenarDesc, setOrdenarDesc] = useState(false);
 
     useEffect(() => {
         function obterTarefas() {
             const tarefasDb = localStorage['tarefas'];
             let listaTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            // ordenar
+            if (ordenarAsc) {
+                listaTarefas.sort((t1, t2) => (t1.nome.toLowerCase() > t2.nome.toLowerCase()) ? 1 : -1);
+            } else if (ordenarDesc) {
+                listaTarefas.sort((t1, t2) => (t1.nome.toLowerCase() < t2.nome.toLowerCase()) ? 1 : -1);
+            }
+            // paginar
             setTotalItems(listaTarefas.length);
             setTarefas(listaTarefas.splice((paginaAtual - 1) * ITENS_POR_PAGINA, ITENS_POR_PAGINA));
         }
@@ -26,10 +36,26 @@ function ListarTarefas() {
             obterTarefas();
             setCarregarTarefas(false);
         }
-    }, [carregarTarefas, paginaAtual]);
+    }, [carregarTarefas, paginaAtual, ordenarAsc, ordenarDesc]);
 
     function handleMudarPagina(pagina) {
         setPaginaAtual(pagina);
+        setCarregarTarefas(true);
+    }
+
+    function handleOrdenar(event) {
+        event.preventDefault();
+        if (!ordenarAsc && !ordenarDesc) {
+            setOrdenarAsc(true);
+            setOrdenarDesc(false);
+        } else if (ordenarAsc) {
+            setOrdenarAsc(false);
+            setOrdenarDesc(true);
+        } else {
+            setOrdenarAsc(false);
+            setOrdenarDesc(false);
+        }
+
         setCarregarTarefas(true);
     }
 
@@ -39,7 +65,13 @@ function ListarTarefas() {
             <Table striped bordered hover responsive data-testid="tabela">
                 <thead>
                     <tr>
-                        <th>Tarefa</th>
+                        <th>
+                            <a href="/" onClick={handleOrdenar}>
+                                Tarefa
+                                &nbsp;
+                                <Ordenacao ordenarAsc={ordenarAsc} ordenarDesc={ordenarDesc} />
+                            </a>
+                        </th>
                         <th>
                             <A href="/cadastrar" className="btn btn-success btn-sm" data-testid="btn-nova-tarefa">
                                 <FontAwesomeIcon icon={faPlus} />
